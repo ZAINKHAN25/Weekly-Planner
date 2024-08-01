290f2bb0e296420cd98788f8b93e716cb4a3aaab
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-08-01 11:44:29 +0500

    Some changes.

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var cards = document.querySelectorAll('.card');
 var modalBody = document.querySelector('.modal-body');
+var modalCard = document.querySelector('.modal-card');
 var body = document.querySelector('body');
+var modalHeading = document.querySelector('.modal-heading');
 console.log(cards);
 cards.forEach(function (card) {
     card.addEventListener('click', function () {
-        modalBody === null || modalBody === void 0 ? void 0 : modalBody.classList.toggle('none');
-        body === null || body === void 0 ? void 0 : body.classList.toggle('overflowHidden');
+        var currentCardHeading = card.querySelector('.heading');
+        if (currentCardHeading) {
+            modalHeading.textContent = currentCardHeading.textContent;
+            modalBody.classList.toggle('none');
+            body.classList.toggle('overflowHidden');
+        }
     });
 });
 modalBody === null || modalBody === void 0 ? void 0 : modalBody.addEventListener('click', function () {
-    modalBody === null || modalBody === void 0 ? void 0 : modalBody.classList.add('none');
-    body === null || body === void 0 ? void 0 : body.classList.remove('overflowHidden');
+    modalBody.classList.add('none');
+    body.classList.remove('overflowHidden');
+});
+modalCard === null || modalCard === void 0 ? void 0 : modalCard.addEventListener('click', function () {
+    modalBody.classList.remove('none');
+    body.classList.add('overflowHidden');
 });
--- app.ts
+++ app.ts
@@ -1,16 +1,28 @@
 const cards = document.querySelectorAll('.card');
 const modalBody = document.querySelector('.modal-body');
+const modalCard = document.querySelector('.modal-card');
 const body = document.querySelector('body');
+const modalHeading = document.querySelector('.modal-heading');
+
 console.log(cards);
 
 cards.forEach(card => {
     card.addEventListener('click', () => {
-        modalBody?.classList.toggle('none');
-        body?.classList.toggle('overflowHidden');
+        const currentCardHeading = card.querySelector('.heading');
+        if (currentCardHeading) {
+            modalHeading.textContent = currentCardHeading.textContent;
+            modalBody.classList.toggle('none');
+            body.classList.toggle('overflowHidden');
+        }
     });
-})
+});
 
 modalBody?.addEventListener('click', () => {
-    modalBody?.classList.add('none');
-    body?.classList.remove('overflowHidden');
+    modalBody.classList.add('none');
+    body.classList.remove('overflowHidden');
+});
+
+modalCard?.addEventListener('click', () => {
+    modalBody.classList.remove('none');
+    body.classList.add('overflowHidden');
 });

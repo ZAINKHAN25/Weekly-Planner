d2dc9b769542a3fcc0526a54884e59cb2a139c1a
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-08-02 11:50:15 +0500

    Some Code Changes.

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-var cards = document.querySelectorAll('.card');
-var modalBody = document.querySelector('.modal-body');
-var body = document.querySelector('body');
-var modalHeading = document.querySelector('.modal-heading');
+var cards = document.querySelectorAll(".card");
+var modalBody = document.querySelector(".modal-body");
+var body = document.querySelector("body");
+var modalHeading = document.querySelector(".modal-heading");
+var modalCloseBtn = document.querySelector(".modal-close-btn");
 console.log(cards);
 cards.forEach(function (card) {
-    card.addEventListener('click', function () {
-        var currentCardHeading = card.querySelector('.heading');
+    card.addEventListener("click", function () {
+        var currentCardHeading = card.querySelector(".heading");
         if (currentCardHeading) {
             modalHeading.textContent = currentCardHeading.textContent;
-            modalBody.classList.toggle('none');
-            body.classList.toggle('overflowHidden');
+            modalBody.classList.toggle("none");
+            body.classList.toggle("overflowHidden");
         }
     });
+});
+modalCloseBtn === null || modalCloseBtn === void 0 ? void 0 : modalCloseBtn.addEventListener("click", function () {
+    modalBody.classList.add("none");
+    body.classList.remove("overflowHidden");
 });
--- app.ts
+++ app.ts
@@ -1,17 +1,23 @@
-const cards = document.querySelectorAll('.card');
-const modalBody = document.querySelector('.modal-body');
-const body = document.querySelector('body');
-const modalHeading = document.querySelector('.modal-heading');
+const cards = document.querySelectorAll(".card");
+const modalBody = document.querySelector(".modal-body");
+const body = document.querySelector("body");
+const modalHeading = document.querySelector(".modal-heading");
+const modalCloseBtn = document.querySelector(".modal-close-btn");
 
 console.log(cards);
 
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        const currentCardHeading = card.querySelector('.heading');
-        if (currentCardHeading) {
-            modalHeading.textContent = currentCardHeading.textContent;
-            modalBody.classList.toggle('none');
-            body.classList.toggle('overflowHidden');
-        }
-    });
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    const currentCardHeading = card.querySelector(".heading");
+    if (currentCardHeading) {
+      modalHeading.textContent = currentCardHeading.textContent;
+      modalBody.classList.toggle("none");
+      body.classList.toggle("overflowHidden");
+    }
+});
+});
+
+modalCloseBtn?.addEventListener("click", () => {
+    modalBody.classList.add("none");
+    body.classList.remove("overflowHidden");
 });
